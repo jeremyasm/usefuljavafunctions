cc5396658672e8a0cb7b39e2d126172baef41d78
Li Yaoxing <liyaoxing1991@126.com>
2016-08-19 13:15:33 +0800

    make getFirstTimestampOfWeek and getLastTimestampOfWeek smarter

--- src/com/jeremyasm/test/WeekTest.java
+++ src/com/jeremyasm/test/WeekTest.java
@@ -2,11 +2,14 @@ package com.jeremyasm.test;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
 import java.text.ParseException;
+import java.util.TimeZone;
 
 import org.junit.Test;
 
 import com.jeremyasm.util.time.Week;
+import com.jeremyasm.util.time.FormatConverter;
 
 public class WeekTest {
 
@@ -28,14 +31,37 @@ public class WeekTest {
 	public void testGetLastTimestampOfWeekLong() throws ParseException {
 		String timestamp = "1471970942000";
 		String lastTs = Week.getLastTimestampOfWeek(timestamp);
-		assertEquals("1472399999000", lastTs);
+		assertEquals("1472399999999", lastTs);
 	}
 
 	@Test
 	public void testGetLastTimestampOfWeekString() throws ParseException {
 		long timestamp = 1471970942000L;
 		long lastTs = Week.getLastTimestampOfWeek(timestamp);
-		assertEquals(1472399999000L, lastTs);
+		assertEquals(1472399999999L, lastTs);
+	}
+		
+	@Test
+	public void testGetFirstAndLastTsOfTheWeek() throws ParseException{
+		
+		long timestamp = 1472749323000L; //2016-9-1 01:02:03 THU
+		long firstTs = Week.getFirstTimestampOfWeek(timestamp);
+		long lastTs = Week.getLastTimestampOfWeek(timestamp);
+		assertEquals(FormatConverter.convertToDateString(firstTs+""),"2016-08-29 00:00:00");
+		assertEquals(FormatConverter.convertToDateString(lastTs+""),"2016-09-04 23:59:59");
+		
+		timestamp = 1472490123000L; //2016-8-30 01:02:03 TUE
+		firstTs = Week.getFirstTimestampOfWeek(timestamp);
+		lastTs = Week.getLastTimestampOfWeek(timestamp);
+		assertEquals(FormatConverter.convertToDateString(firstTs+""),"2016-08-29 00:00:00");
+		assertEquals(FormatConverter.convertToDateString(lastTs+""),"2016-09-04 23:59:59");
+		
+		timestamp = 1471539723000L; //2016-8-19 01:02:03 FRI
+		firstTs = Week.getFirstTimestampOfWeek(timestamp);
+		lastTs = Week.getLastTimestampOfWeek(timestamp);
+		assertEquals(FormatConverter.convertToDateString(firstTs+""),"2016-08-15 00:00:00");
+		assertEquals(FormatConverter.convertToDateString(lastTs+""),"2016-08-21 23:59:59");
+
 	}
 
 }
--- src/com/jeremyasm/util/time/Week.java
+++ src/com/jeremyasm/util/time/Week.java
@@ -7,69 +7,89 @@ import java.util.Date;
 
 public class Week {
 	
+	public static int TIMEZONE_DIFF = 8; //0 for UK, 1 for Germany, 8 for China
+	public static long HOUR_IN_MS = 3600000L; //how many milliseconds in one hour
+	public static long DAY_IN_MS = 86400000L; //how many milliseconds in one day
+	public static long WEEK_IN_MS = 604800000L; //How many milliseconds in one week
+	public static long WEEKDAY_OFFSET = 3* DAY_IN_MS + TIMEZONE_DIFF * HOUR_IN_MS; ////1970-01-01 is Thursday, and the 2nd part is about time-zone
+
 	/**
-	 * Get the first timestamp of the week
+	 * Get the first timestamp of the week (Smart Version)
 	 * @param timestamp
 	 * @return firstTimestamp, the first timestamp of Monday of this week
 	 * @throws ParseException
 	 */
 	public static long getFirstTimestampOfWeek(long timestamp) throws ParseException{
 
-		SimpleDateFormat dayFormat =  new SimpleDateFormat("yyyy-MM-dd");  
-		String formatedTs = dayFormat.format(timestamp);  
-	    Date date = dayFormat.parse(formatedTs); 
-	    Calendar cal = Calendar.getInstance(); 
-		cal.setTime(date);
-		cal.setFirstDayOfWeek(Calendar.MONDAY);   
-		cal.set(Calendar. DAY_OF_WEEK, Calendar.MONDAY);  
-		long firstTimestamp = cal.getTimeInMillis();
-		return firstTimestamp;
-	}
-	
-	
-	/**
-	 * Get the first timestamp of the week
-	 * @param timestamp
-	 * @return firstTimestamp, the first timestamp of Monday of this week
-	 * @throws ParseException
-	 */
-	public static String getFirstTimestampOfWeek(String timestamp) throws ParseException{
-		return getFirstTimestampOfWeek(Long.parseLong(timestamp))+"";
+		long firstTs = timestamp - (timestamp + WEEKDAY_OFFSET) % WEEK_IN_MS;
+		return firstTs;
+		
 	}
 
-	
 	/**
-	 * Get the last timestamp of the week
+	 * Get the last timestamp of the week (Smart Version)
 	 * @param timestamp
 	 * @return lastTimestamp, the last timestamp of Sunday of this week
 	 * @throws ParseException
 	 */
 	public static long getLastTimestampOfWeek(long timestamp) throws ParseException{
 
-		SimpleDateFormat dayFormat =  new SimpleDateFormat("yyyy-MM-dd");  
-		String formatedTs = dayFormat.format(timestamp);  
-	    Date date = dayFormat.parse(formatedTs); 
-	    Calendar cal = Calendar.getInstance();  
-	    int day_of_week = cal.get(Calendar. DAY_OF_WEEK) - 1;  
-	    if (day_of_week == 0 ) {  
-	        day_of_week = 7 ;  
-	    }  
-	    cal.setTime(date);
-	    cal.add(Calendar.DATE , -day_of_week + 7);  
-	    long lastTimestamp = cal.getTimeInMillis() + 86399000;
-	    return lastTimestamp;
+		long lastTs = getFirstTimestampOfWeek(timestamp) + WEEK_IN_MS - 1;
+		return lastTs;
+		
 	}
 	
+	//input String, return String
+	public static String getFirstTimestampOfWeek(String timestamp) throws ParseException{
+		return getFirstTimestampOfWeek(Long.parseLong(timestamp))+"";
+	}
 	
-	/**
-	 * Get the last timestamp of the week
-	 * @param timestamp
-	 * @return lastTimestamp, the last timestamp of Sunday of this week
-	 * @throws ParseException
-	 */
+	//input String, return String
 	public static String getLastTimestampOfWeek(String timestamp) throws ParseException{
 		return getLastTimestampOfWeek(Long.parseLong(timestamp))+"";
 	}
 	
+//	/**
+//	 * Get the first timestamp of the week (STUPID VERSION, see the smart version above.)
+//	 * @param timestamp
+//	 * @return firstTimestamp, the first timestamp of Monday of this week
+//	 * @throws ParseException
+//	 */
+//	public static long getFirstTimestampOfWeek(long timestamp) throws ParseException{
+//
+//		SimpleDateFormat dayFormat =  new SimpleDateFormat("yyyy-MM-dd");  
+//		String formatedTs = dayFormat.format(timestamp);  
+//	    Date date = dayFormat.parse(formatedTs); 
+//	    Calendar cal = Calendar.getInstance(); 
+//		cal.setTime(date);
+//		cal.setFirstDayOfWeek(Calendar.MONDAY);   
+//		cal.set(Calendar. DAY_OF_WEEK, Calendar.MONDAY);  
+//		long firstTimestamp = cal.getTimeInMillis();
+//		return firstTimestamp;
+//	}
+//
+//	/**
+//	 * Get the last timestamp of the week (STUPID VERSION, see the smart version above.)
+//	 * @param timestamp
+//	 * @return lastTimestamp, the last timestamp of Sunday of this week
+//	 * @throws ParseException
+//	 */
+//	public static long getLastTimestampOfWeek(long timestamp) throws ParseException{
+//
+//		SimpleDateFormat dayFormat =  new SimpleDateFormat("yyyy-MM-dd");  
+//		String formatedTs = dayFormat.format(timestamp);  
+//	    Date date = dayFormat.parse(formatedTs); 
+//	    Calendar cal = Calendar.getInstance();  
+//	    int day_of_week = cal.get(Calendar. DAY_OF_WEEK) - 1;  
+//	    if (day_of_week == 0 ) {  
+//	        day_of_week = 7 ;  
+//	    }  
+//	    cal.setTime(date);
+//	    cal.add(Calendar.DATE , -day_of_week + 7);  
+//	    long lastTimestamp = cal.getTimeInMillis() + 86399000;
+//	    return lastTimestamp;
+//	}
+//	
+//	
 
 }
